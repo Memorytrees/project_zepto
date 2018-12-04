7a7606566f4d90cfd9df7c17422abc398420849a
benabree.he@gmail.com <benabree.he@gmail.com>
2018-12-04 15:06:56 +0800

    0011

--- js/index.js
+++ js/index.js
@@ -1,125 +1,126 @@
-
-/**
- * Created by zhiyongYan on 2017/3/6.
- */
 $(function () {
-	//初始化坐标变量
-	var last = {row:0,col:0};
-	var now = {row:1,col:1};
-	//初始化四个方向变量
-	var direction = {up:1,right:2,down:3,left:4};
-	//初始化是否移动变量
-	var isMoving = false;
+    // 初始化数据
+    var direction = {up: 1, right: 2, down: 3, left: 4}
+    // 初始化两个坐标
+    var now = {col: 1, row: 1};
+    var last = {col: 0, row: 0};
+
+    // 初始化变量表示页面没有在滑动
+    var isMoving = false;
+
+    // 向上滑动
+    $('.page').swipeUp(function () {
+        // 判断页面是否在滑动
+        if(isMoving) {
+            return;
+        }
+        // 计算滑动之后lastPage页面的坐标
+        last.col = now.col;
+        last.row = now.row;
+        if(last.col < 5) {
+            // 计算滑动之后进厂页面的坐标
+            now.col = last.col + 1;
+            now.row = last.row;
+            movePage(direction.up);
+        }
+        
+    });
+    // 向下滑动
+    $('.page').swipeDown(function () {
+        // 判断页面是否在滑动
+        if(isMoving) {
+            return;
+        }
+        // 计算滑动之后lastPage页面的坐标
+        last.col = now.col;
+        last.row = now.row;
+        if(last.col > 1) {
+            // 计算滑动之后进场页面的坐标
+            now.col = last.col - 1;
+            now.row = last.row;
+            movePage(direction.down);
+        }
+    });
+
+    // 向左滑动
+    $('.page').swipeLeft(function () {
+        // 判断页面是否在滑动
+        if(isMoving) {
+            return;
+        }
+        // 计算滑动之后lastPage页面的坐标
+        last.col = now.col;
+        last.row = now.row;
+        if(last.col > 1 && last.col < 5 && last.row == 1) {
+            // 计算滑动之后进场页面的坐标
+            now.col = last.col;
+            now.row = last.row + 1;
+            movePage(direction.left);
+        }
+    });
+
+    // 向右滑动
+    $('.page').swipeRight(function () {
+        // 判断页面是否在滑动
+        if(isMoving) {
+            return;
+        }
+        // 计算滑动之后lastPage页面的坐标
+        last.col = now.col;
+        last.row = now.row;
+        if(last.col > 1 && last.col < 5 && last.row == 2) {
+            // 计算滑动之后进场页面的坐标
+            now.col = last.col;
+            now.row = last.row - 1;
+            movePage(direction.right);
+        }
+    });
 
 
-	//向上滑动
-	$(document).swipeUp(function () {
-		if(isMoving){
-			return;
-		}
-		//将滑动前手指作用的页面的坐标赋值给滑动之后上一个页面
-		last.row = now.row;
-		last.col = now.col;
-		//判断滑动的条件  滑动所需参数的设置
-		if(last.col!=5){
-			now.row = 1;
-			now.col = last.col + 1;
-			pageMove(direction.up);
-		}
-	});
+    // 定义一个滑动的函数
+    function movePage(dir) {
+        // 初始化参与动画的页面
+        var lastPage = '.page-' + last.col + '-' + last.row;
+        var nowPage = '.page-' + now.col + '-' + now.row;
+        // 初始化两个动画类
+        var inClass = '';   // 进场的动画类
+        var outClass = '';  // 出厂的动画类
+        // 匹配方向
+        switch (dir) {
+            case direction.up:
+                outClass = 'pt-page-moveToTop';
+                inClass = 'pt-page-moveFromBottom';
+                break;
+            case direction.right:
+                outClass = 'pt-page-moveToRight';
+                inClass = 'pt-page-moveFromLeft';
+                break;   
+            case direction.down:
+                outClass = 'pt-page-moveToDown';
+                inClass = 'pt-page-moveFromTop';
+                break;
+            case direction.left:
+                outClass = 'pt-page-moveToLeft';
+                inClass = 'pt-page-moveFromRight';
+                break;      
+        }
+        // 将动画类加到参与的动画的页面上
+        $(lastPage).addClass(outClass);
+        $(nowPage).removeClass('hide');
+        $(nowPage).addClass(inClass);
+        isMoving = true;
 
-	//向下滑动
-	$(document).swipeDown(function () {
-		if(isMoving){
-			return;
-		}
-		//将滑动前手指作用的页面的坐标赋值给滑动之后上一个页面
-		last.row = now.row;
-		last.col = now.col;
-		//判断滑动的条件  滑动所需参数的设置
-		if(last.col!=1){
-			now.row = 1;
-			now.col = last.col - 1;
-			pageMove(direction.down);
-		}
-	});
-
-	//向左滑动
-	$(document).swipeLeft(function () {
-		if(isMoving){
-			return;
-		}
-		//将滑动前手指作用的页面的坐标赋值给滑动之后上一个页面
-		last.row = now.row;
-		last.col = now.col;
-		//判断滑动的条件  滑动所需参数的设置
-		if(last.col>1&&last.col<5&&last.row==1){
-			now.row = last.row + 1;
-			pageMove(direction.left);
-		}
-	});
-
-	//向右滑动
-	$(document).swipeRight(function () {
-		if(isMoving){
-			return;
-		}
-		//将滑动前手指作用的页面的坐标赋值给滑动之后上一个页面
-		last.row = now.row;
-		last.col = now.col;
-		//判断滑动的条件  滑动所需参数的设置
-		if(last.col>1&&last.col<5&&last.row==2){
-			now.row = last.row - 1;
-			pageMove(direction.right);
-		}
-	});
-	//定义move函数
-	function pageMove(dir) {
-		//初始化作用的两个页面
-		var lastPage = '.page-' + last.col + '-' + last.row;
-		var nowPage = '.page-' + now.col + '-' + now.row;
-
-		//初始化页面类
-		var outClass = '';
-		var inClass = '';
-		//匹配方向
-		switch (dir){
-			case direction.up:
-				outClass = 'pt-page-moveToTop';
-				inClass = 'pt-page-moveFromBottom';
-				break;
-			case direction.right:
-				outClass = 'pt-page-moveToRight';
-				inClass = 'pt-page-moveFromLeft';
-				break;
-			case direction.down:
-				outClass = 'pt-page-moveToBottom';
-				inClass = 'pt-page-moveFromTop';
-				break;
-			case direction.left:
-				outClass = 'pt-page-moveToLeft';
-				inClass = 'pt-page-moveFromRight';
-				break;
-		}
-		//为页面添加动画开始的类
-		$(nowPage).removeClass('hide');
-
-		$(lastPage).addClass(outClass);
-		$(nowPage).addClass(inClass);
-		isMoving = true;
-		//处理动画完成时页面的类
-		setTimeout(function () {
-			$(lastPage).addClass('hide');
-			$(lastPage).removeClass(outClass);
-			$(lastPage).removeClass('page-current');
-			$(lastPage).find('img').addClass('hide');
-
-			$(nowPage).addClass('page-current');
-			$(nowPage).removeClass(inClass);
-			$(nowPage).find('img').removeClass('hide');
-			isMoving = false;
-		},600);
-	}
-});
-
-
+        // 动画执行完清除动画类/扫尾工作
+        setTimeout(function () {
+            $(lastPage).removeClass(outClass);
+            $(lastPage).addClass('hide');
+            $(lastPage).removeClass('page-current');
+            $(lastPage).find('img').addClass('hide');
+            $(nowPage).find('img').removeClass('hide');
+            $(nowPage).removeClass(inClass);
+            $(nowPage).addClass('page-current');
+            isMoving = false;
+        },600)
+        
+    }
+})
